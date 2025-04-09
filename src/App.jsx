1d2b9b779b96b3896dc68e50c62ea15c99886a0a
andreapanico12 <andreapanico12@gmail.com>
2025-04-09 12:12:52 +0200

    add search functionality to filter politicians by name

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import PoliticianCard from "./components/PoliticianCard";
 
 function App() {
 
   const [politicians, setPoliticians] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
 
   const fetchPoliticians = async () => {
     const response = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/politicians`);
@@ -14,13 +16,28 @@ function App() {
     fetchPoliticians();
   }
   , []);
+
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value.toLowerCase());  
+  }
+
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter((politician => politician.name.toLowerCase().includes(searchTerm) ))
+  }, [politicians, searchTerm]);
+  
+
   
 
   return (
     <>
     <h1>POLITICIANS LIST:</h1>
+    <div>
+      <label htmlFor="search"><p><strong>Cerca il politico:</strong></p></label>
+      <input id="search" type="text" value={searchTerm} onChange={handleSearch} placeholder="Search for a politician..." />
+    </div>
     <div className="politicians-card-container">
-      {politicians.map((politician) => < PoliticianCard key={politician.id} politician={politician}/>)}
+      {filteredPoliticians.map((politician) => < PoliticianCard key={politician.id} politician={politician}/>)}
     </div>
     </>
   )
