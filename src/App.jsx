eacbab06e7746e4df02f50305e3deb136fafc876
andreapanico12 <andreapanico12@gmail.com>
2025-04-09 12:45:56 +0200

    add position filter to politician search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ function App() {
 
   const [politicians, setPoliticians] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-
+  const [position, setPosition] = useState("");
+  
+  const uniquePositions = [];
+  politicians.forEach(politician => {
+    if (!uniquePositions.includes(politician.position)) {
+      uniquePositions.push(politician.position);
+    }
+  });
+  console.log(uniquePositions);
 
   const fetchPoliticians = async () => {
     const response = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/politicians`);
@@ -23,8 +31,8 @@ function App() {
   }
 
   const filteredPoliticians = useMemo(() => {
-    return politicians.filter((politician => politician.name.toLowerCase().includes(searchTerm) ))
-  }, [politicians, searchTerm]);
+    return politicians.filter((politician => politician.name.toLowerCase().includes(searchTerm) && (position ? politician.position === position : true)));
+  }, [politicians, searchTerm , position]);
   
 
   
@@ -35,6 +43,16 @@ function App() {
     <div>
       <label htmlFor="search"><p><strong>Cerca il politico:</strong></p></label>
       <input id="search" type="text" value={searchTerm} onChange={handleSearch} placeholder="Search for a politician..." />
+      <label htmlFor="select"><p><strong>Posizione: </strong></p></label>
+      <select id="select " value={position} onChange={(e) => setPosition(e.target.value)}>
+        <option value="">All</option>
+        {uniquePositions.map((position, index) => {
+          return (
+            <option key={index} value={position}>{position}</option>
+          )
+        }
+        )}
+         </select>
     </div>
     <div className="politicians-card-container">
       {filteredPoliticians.map((politician) => < PoliticianCard key={politician.id} politician={politician}/>)}
